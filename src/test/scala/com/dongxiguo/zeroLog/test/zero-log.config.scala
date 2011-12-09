912b1561a7f46d5a727628e527dcc18c07c4562a
yangbo <yangbo@localhost>
2011-12-09 17:22:06 +0800

    ??sbt??

--- src/test/scala/com/dongxiguo/zeroLog/test/zero-log.config.scala
+++ src/test/scala/com/dongxiguo/zeroLog/test/zero-log.config.scala
@@ -1,13 +1,13 @@
 // vim: expandtab shiftwidth=2 softtabstop=2
 /*
  * Copyright 2011 杨博 (Yang Bo)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,8 +15,6 @@
  * limitations under the License.
  */
 
-// vim: expandtab shiftwidth=2 softtabstop=2
-import com.dongxiguo.zeroLog.Logger
 import com.dongxiguo.zeroLog.Filter
 import com.dongxiguo.zeroLog.formatters.SimpleFormatter
 import scala.util.logging.ConsoleLogger
